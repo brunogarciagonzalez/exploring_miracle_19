fff6a43033692ddb042de7938c065c1bfdc1ae7d
Bruno Garcia Gonzalez <brunogarciagonzalez@outlook.com>
2022-08-11 10:34:00 -0400

    added print statements to # 5, 6, 7

--- numerical_combination_6.rb
+++ numerical_combination_6.rb
@@ -19,6 +19,8 @@ COLLECTION.each do |tuple|
   @output += concatenation_of_chapter_verse_numbers
 end
 
+puts @output
+
 # @output turns out to be the following number, which was confrmed to be divisible by 19 using:
 # https://www.dcode.fr/modulo-n-calculator
 
